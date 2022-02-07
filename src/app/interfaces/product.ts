dba3eec83b80aeeeb1c0b6139a97639f641e3aff
franmb93 <fcomunozbetanzos@gmail.com>
2022-02-07 22:15:48 +0100

    1/2 navbar

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -16,5 +16,3 @@ export interface Product {
   category: Category;
   deal: Deal;
 }
-
-//TODO falta deal y fechas
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+
+
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-
   ],
   exports: [
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule
   ]
 })
 export class MaterialModule { }
