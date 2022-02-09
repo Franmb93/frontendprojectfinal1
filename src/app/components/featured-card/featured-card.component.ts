bece2fbf73f8c93e76afc9b8e32a14adc92232b5
guillermico <gupinero@capgemini.com>
2022-02-09 20:44:04 +0100

    inicio ok a falta de pasar los datos de bbdd

--- src/app/components/featured-card/featured-card.component.ts
+++ src/app/components/featured-card/featured-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-featured-card',
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FeaturedCardComponent implements OnInit {
 
   constructor() { }
+
+  @Input() item: any;
 
   ngOnInit(): void {
   }
--- src/app/components/featured-products/featured-products.component.ts
+++ src/app/components/featured-products/featured-products.component.ts
@@ -9,7 +9,13 @@ export class FeaturedProductsComponent implements OnInit {
 
   featuredProductsOne = [
     {name: "hdmi", description: "un cable"},
-    {name: "hdmi", description: "un cable"},
+    {name: "impresora", description: "un cable"},
+    {name: "Docking Station", description: "Un dock pa conectar toda cosa chico"},
+  ]
+
+  featuredProductsTwo = [
+    {name: "adidas", description: "zapas"},
+    {name: "camiseta", description: "de usa chan"},
     {name: "hdmi", description: "un cable"},
   ]
 
