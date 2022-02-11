1326f08409d6112436bf49cb014520b9c264f1dd
Daniel Castillo <daniel.castillo-tamayo@capgemini.com>
2022-02-11 08:18:08 +0100

    Semiorganizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material/material.module';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './components/shared/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './components/home/home.component';
 import { CategoryBannerComponent } from './components/category-banner/category-banner.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 import { CategoryComponent } from './components/category/category.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { SearchBarComponent } from './components/shared/search-bar/search-bar.component';
 import { FeaturedProductsComponent } from './components/featured-products/featured-products.component';
 import { FeaturedCardComponent } from './components/featured-card/featured-card.component';
 import { MatButtonModule } from '@angular/material/button';
 import { ProductModule } from './components/product/product.module';
-import { ProductCardComponent } from './components/product/product-card/product-card.component';
+import { GalleryModule } from './modules/gallery/gallery.module';
+import { ProductDetailsModule } from './modules/product-details/product-details.module';
 
 
 @NgModule({
@@ -38,7 +39,9 @@ import { ProductCardComponent } from './components/product/product-card/product-
     ProductModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    ProductModule
+    ProductModule,
+    GalleryModule,
+    ProductDetailsModule
     ],
   
 
