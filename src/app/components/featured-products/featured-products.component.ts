492941fe29b29c585198676c6cd61f6e1291aa0b
Daniel Castillo <daniel.castillo-tamayo@capgemini.com>
2022-02-11 17:58:38 +0100

    - Gallery display apropiado practicamente terminado - Comentar parte de Featured Products - Limpiando ProductCard

--- src/app/components/featured-products/featured-products.component.ts
+++ src/app/components/featured-products/featured-products.component.ts
@@ -7,17 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturedProductsComponent implements OnInit {
 
-  featuredProductsOne = [
-    {name: "hdmi", description: "un cable"},
-    {name: "impresora", description: "un cable"},
-    {name: "Docking Station", description: "Un dock pa conectar toda cosa chico"},
-  ]
+//   featuredProductsOne = [
+//     {name: "hdmi", description: "un cable"},
+//     {name: "impresora", description: "un cable"},
+//     {name: "Docking Station", description: "Un dock pa conectar toda cosa chico"},
+//   ]
 
-  featuredProductsTwo = [
-    {name: "adidas", description: "zapas"},
-    {name: "camiseta", description: "de usa chan"},
-    {name: "hdmi", description: "un cable"},
-  ]
+//   featuredProductsTwo = [
+//     {name: "adidas", description: "zapas"},
+//     {name: "camiseta", description: "de usa chan"},
+//     {name: "hdmi", description: "un cable"},
+//   ]
 
   constructor() { }
 
--- src/app/modules/gallery/gallery.component.ts
+++ src/app/modules/gallery/gallery.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
 	selector: 'app-gallery',
@@ -11,35 +12,43 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class GalleryComponent implements OnInit {
 	
-	
 	products: Product[] = [];
+
+	isUserType: boolean = false;
 	
 	constructor(
 		private route: ActivatedRoute,
 		private categoryService: CategoryService,
+		private userService: UserService,
 		private productService: ProductService) {
 			
 			this.route.data.subscribe(
 				data => {
 
-					console.log(data['type']);
+					// console.log(data['type']);
 
 					switch (data['type']) {
 						case 'results':
-							this.getProducts();
+							this.getProducts(); // TODO metodo que regrese resultados de la busqueda
+							
 							break;
 						case 'category':
 							this.getProductsByCategory(data["category"]);
+
 							break;
 						case 'user':
+							this.isUserType = true;
 							this.route.params.subscribe(
 								params => {
-									console.log(params);
+									this.getProductsByUser(+params['id'])
 								}
 							);
+							
 							break;
 						default:
 							this.getProducts();
+
+
 							break;
 					}
 					
@@ -52,15 +61,37 @@ export class GalleryComponent implements OnInit {
 		
 	}
 	
-	getProductsByCategory(category: string) {
-		// this.categoryService.getCategory
-	}
-	
 	getProducts() {
 		this.productService.getProducts().subscribe(
 			(response) => {
 				this.products = response._embedded.productList;
 			});
+	}
+
+	getProductsByCategory(category: string) {
+		// this.categoryService.getCategory
+		switch (category) {
+			case "ropa":
+				this.categoryService.getCategory(1).subscribe(
+					category => {
+						this.products = category.products;
+						// console.log(this.products);
+					}
+				)
+				break;
+		
+			default:
+				break;
+		}
+	}
+
+	getProductsByUser(id: number) {
+		this.userService.getUser(id).subscribe(
+			user => {
+				this.products = user.products;
+				// console.log(this.products);
+			}
+		)
 	}
 		
 }
