643af24498599684920c7055e0eee7ed3236b603
Daniel Castillo <daniel.castillo-tamayo@capgemini.com>
2022-02-14 21:32:23 +0100

    - Purchase funcionalidad acabada

--- src/app/components/modals/purchase/purchase.component.ts
+++ src/app/components/modals/purchase/purchase.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Deal } from 'src/app/interfaces/deal';
 import { Product } from 'src/app/interfaces/product';
 import { User } from 'src/app/interfaces/user';
+import { DealService } from 'src/app/services/deal.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -15,7 +17,7 @@ export class PurchaseComponent implements OnInit {
 	public owner!: User;
 	public self!: User;
 
-	constructor(private service: UserService, public dialogRef: MatDialogRef<PurchaseComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
+	constructor(private service: UserService, private dealService: DealService, public dialogRef: MatDialogRef<PurchaseComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { }
 	
 	ngOnInit(): void {
 		this.product = this.data['product'];
@@ -38,11 +40,31 @@ export class PurchaseComponent implements OnInit {
 	}
 
 	updateWallets(): void {
+		this.self.wallet -= this.product.price;
+		this.service.putUser(this.self.id, this.self).subscribe();
 
+		this.owner.wallet += this.product.price;
+		this.service.putUser(this.owner.id, this.owner).subscribe();
 	}
 
 	postDeal(): void {
 
+		
+		const deal = {
+			price: this.product?.price,
+			product: { id: this.product.id },
+			user: { id: this.self.id }
+		}
+
+		console.log(deal);
+		
+
+		this.dealService.postDeal(deal).subscribe(
+			response => {
+				console.log(response);
+				
+			}
+		);
 	}
 	
 }
--- src/app/services/deal.service.ts
+++ src/app/services/deal.service.ts
@@ -18,4 +18,10 @@ export class DealService {
 	getDeal(id: any): Observable<any>{		
 		return this.http.get<any>(`${this.url}/${id}`);
 	}
+
+	postDeal(body: any): Observable<any> {
+		const headers = { 'Content-Type': 'Application/json' };
+
+		return this.http.post<any>(this.url, body, { headers: headers });
+	}
 }
