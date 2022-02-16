71ff55a68f7aac41753055fb39f58c9fd84761a3
Daniel Castillo <daniel.castillo-tamayo@capgemini.com>
2022-02-16 11:40:06 +0100

    Arreglado CSS pt2

--- src/app/components/featured-products/featured-products.component.ts
+++ src/app/components/featured-products/featured-products.component.ts
@@ -6,50 +6,51 @@ import { ProductService } from 'src/app/services/product.service';
 import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-featured-products',
-  templateUrl: './featured-products.component.html',
-  styleUrls: ['./featured-products.component.scss']
+	selector: 'app-featured-products',
+	templateUrl: './featured-products.component.html',
+	styleUrls: ['./featured-products.component.scss']
 })
 export class FeaturedProductsComponent implements OnInit {
-
-
-  public id!: number;
+	
+	
+	public id!: number;
 	public product!: Product;
 	public image!: string;
-
+	
 	products1: Product[] = [];
-  products2: Product[] = [];
-
-  constructor(
-    private service: ProductService,
-	  private route: ActivatedRoute
-  ) { }
-
-  ngOnInit(): void {
-    this.getProducts()
-  }
-
-
-  getProducts() {
-    let featuredProducts = []
+	products2: Product[] = [];
+	
+	constructor(
+		private service: ProductService,
+		private route: ActivatedRoute
+	) { }
+	
+	ngOnInit(): void {
+		this.getProducts()
+	}
+	
+	
+	getProducts() {
+		let featuredProducts = []
 		this.service.getProducts().subscribe(
 			data => {
-        // this.products = data._embedded.productList
-        featuredProducts = data._embedded.productList;
-        for (let i = 0; i<3; i ++) {
-          this.products1.push(featuredProducts[i])
-        }
-        // for (let i = 3; i<6; i ++) {
-        //   this.products2.push(featuredProducts[i])
-        // }
-        for (let i = 0; i<featuredProducts.length; i ++) {
-          this.products2.push(featuredProducts[i]) 
-        }
+				
+				featuredProducts = data._embedded.productList;
+
+				for (let i = 0; i < 3; i++) {
+					this.products1.push(featuredProducts[i])
+					console.log(featuredProducts[i]);
+					
+				}
+				
+				for (let i = 3; i < 6; i++) {
+					if(featuredProducts[i]) {
+						this.products2.push(featuredProducts[i]);
+					}
+				}
 			}
 		);
 	}
-
-
-
-
+	
 }
+		
